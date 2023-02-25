24e63d468c50d530c6303958009e91f33fc2484c
AlirezaGhadyani <alirezaghadyani16@gmail.com>
2023-02-25 11:24:00 +0330

    fix(.npmignore): add .npmignore file to ignore (`.github`, `src`) folders to reduce unpacked size

--- dist/utils/index.d.ts
+++ dist/utils/index.d.ts
@@ -1,4 +1,4 @@
 export { default as isBrowser } from './isBrowser';
 export { default as sessionStorage } from './sessionStorage';
 export { default as localStorage } from './localStorage';
-export { default as cookieStorage } from './cookieStorage';
+export { default as cookieStorage } from 'js-cookie';
